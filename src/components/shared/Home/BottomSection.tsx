812908cf04529d2f73a609fe884d8c5c87b3c3b7
MrEpoch <alexandrstencuk@seznam.cz>
2024-06-23 22:49:29 +0200

    feat: Divider added
    Added divider
    
    Later will work on responsivness like adding hamburger menu for nav on
    small screens and text sizing for smaller devices
    
    Added table for information about location, phone and mail

--- src/components/shared/Home/BottomSection.tsx
+++ src/components/shared/Home/BottomSection.tsx
@@ -1,20 +1,49 @@
 import { Mail, MapPin, Phone } from "lucide-react";
 import Image from "next/image";
 import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 export default function BottomSection() {
+  const coffeCardData = [
+    {
+      text: "Berounová 521, Praha 3",
+    Svg: MapPin,
+    },
+    {
+      text: "+420 123 456 789",
+    Svg: Phone,
+    },
+    {
+      text: "example@example.example",
+    Svg: Mail,
+    },
+  ]
+
   return (
     <section className="w-full py-16 text-white flex flex-col justify-center items-center gap-10 pt-48 min-h-screen">
-      <h1 className="text-5xl font-bold font-anton">
+      <h1 className="lg:text-5xl text-2xl sm:text-3xl font-bold font-anton text-center">
         Přijď otestovat naší kávu
       </h1>
       <div className="flex lg:flex-row flex-col items-center justify-between h-full gap-8 w-full">
-        <Image src="/coffee.png" alt="coffee" width={300} height={300} />
-        <div className="bg-gray-50 py-10 px-16 flex flex-col gap-6 rounded-md">
-          <CoffeCardElement Svg={MapPin} text="Berounová 521, Praha 3" />
-          <CoffeCardElement Svg={Phone} text="+420 111 111 111" />
-          <CoffeCardElement Svg={Mail} text="mail@mail.mail" />
-        </div>
+        <div className="w-full flex items-center justify-center"><Image src="/coffee.png" alt="coffee" width={300} height={300} /></div>
+    <Table>
+        <TableBody>
+    {coffeCardData.map((data) => (
+      <TableRow className="hover:bg-transparent" key={data.text}>
+        <CoffeCardElement Svg={data.Svg} text={data.text} />
+      </TableRow>
+    ))}
+  </TableBody>
+</Table>
+
       </div>
     </section>
   );
@@ -22,9 +51,10 @@ export default function BottomSection() {
 
 function CoffeCardElement({ Svg, text }) {
   return (
-    <div className="text-black flex items-center gap-4 w-full">
-      <Svg width={30} height={30} />
-      <p>{text}</p>
-    </div>
+      <TableCell className="font-medium flex gap-4 items-center">
+        <Svg width={30} height={30} />
+        <p>{text}</p> 
+      </TableCell>
+
   );
 }
